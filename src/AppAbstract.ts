806b86f6ea1d594ec6984c5a97a5f73de8169ba1
Uriah Ahrak <uriah.achrak@gmail.com>
2018-08-15 16:48:52 +0300

    Added dynamic port

--- src/AppAbstract.ts
+++ src/AppAbstract.ts
@@ -7,11 +7,17 @@ export abstract class AppAbstract {
     public bootstrap() {
 
         this.port = 8400;
+        console.log(this.port);
+        
         if (typeof process.argv[2] !== undefined) {
             this.port = +process.argv[2];
         }
 
         console.log(this.port);
+        console.log(process.argv);
+        console.log(process.argv[2]);
+        console.log(process.argv[3]);
+        console.log(+process.argv[2]);
         
         this.server.listen(this.port, () => {
             console.log('Listening...');
