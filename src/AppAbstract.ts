abf1a79736794b57456ef0ca4c6d9f7f16c16fef
Uriah Ahrak <uriah.achrak@gmail.com>
2018-08-16 21:14:05 +0300

    Added dynamic port choosing

--- src/AppAbstract.ts
+++ src/AppAbstract.ts
@@ -7,17 +7,10 @@ export abstract class AppAbstract {
     public bootstrap() {
 
         this.port = 8400;
-        console.log(this.port);
-        
-        if (typeof process.argv[2] !== undefined) {
+
+        if (typeof process.argv[2] !== 'undefined') {
             this.port = +process.argv[2];
         }
-
-        console.log(this.port);
-        console.log(process.argv);
-        console.log(process.argv[2]);
-        console.log(process.argv[3]);
-        console.log(+process.argv[2]);
         
         this.server.listen(this.port, () => {
             console.log('Listening...');
